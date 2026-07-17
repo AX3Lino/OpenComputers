d374dd53410a5aa80e7978358dfe5a579d2a6899
AX3Lino <ax3lino@gmail.com>
2026-07-17 08:58:43 +0200

    Position informed actuators

--- src/main/scala/li/cil/oc/server/component/MEDualActuator.scala
+++ src/main/scala/li/cil/oc/server/component/MEDualActuator.scala
@@ -6,6 +6,9 @@ import li.cil.oc.Constants
 import li.cil.oc.api
 import li.cil.oc.api.driver.DeviceInfo.DeviceAttribute
 import li.cil.oc.api.driver.DeviceInfo.DeviceClass
+import li.cil.oc.api.machine.Arguments
+import li.cil.oc.api.machine.Callback
+import li.cil.oc.api.machine.Context
 import li.cil.oc.api.network.EnvironmentHost
 import li.cil.oc.api.network.Visibility
 import li.cil.oc.common.tileentity
@@ -29,6 +32,11 @@ object MEDualActuator {
     )
 
     override def getDeviceInfo: util.Map[String, String] = deviceInfo
+
+    // Lets a computer correlate this actuator with whichever driver-backed component ended up on
+    // its side and multiple actuators on one network are otherwise indistinguishable.
+    @Callback(doc = """function():number, number, number -- Get the X, Y, Z position of this block.""")
+    def getCoordinates(context: Context, args: Arguments): Array[AnyRef] = result(position.x, position.y, position.z)
   }
 
   /** Hosted by the ME Dual Actuator block's own tile entity. */
